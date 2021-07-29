a7ad0345f93ee48405422ab1602e4dc6505bbfca
FrancescoCina <francesco.cina04@gmail.com>
2021-07-29 15:46:34 +0200

    Fatto header e main in HTML. Creata struttura dati con array in JS. Stampato array con v-for

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@ Vue.config.devtools = true;
 
 const root = new Vue({
     el: "#root",
-    data:,
-    methods:,
+    data: {
+        linkLogo: "./img/logo.png",
+        todos: [
+            "Compra pane",
+            "Cambia lampadina",
+            "Cibo per il cane",
+            "Prendi soldi",
+            "Compra latte",
+        ]
+    },
+    methods: {
+
+    }
 })
